4caff06269207ea9ee4bbe47b8267935a1f28b22
https://github.com/TaiseiUgawa <taisei8808@gmai.com>
2022-06-25 00:31:11 +0900

    edit

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -93,7 +93,13 @@ class EventController extends Controller
      */
     public function edit(Event $event)
     {
-        //
+        $event = Event::findOrFail($event->id);
+        $eventDate = $event->eventDate;
+        $startTime = $event->startTime;
+        $endTime = $event->endTime;
+
+        return view('manager.events.edit',
+        compact('event', 'eventDate', 'startTime', 'endTime'));
     }
 
     /**
