6b6593cfacdb41c9110a2e1d1bebc816f45defa1
https://github.com/TaiseiUgawa <taisei8808@gmai.com>
2022-06-25 01:15:18 +0900

    update

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -11,11 +11,6 @@ use App\Services\EventService;
 
 class EventController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $events = DB::table('events')
@@ -26,22 +21,11 @@ class EventController extends Controller
         compact('events'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         return view('manager.events.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreEventRequest  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(StoreEventRequest $request)
     {
         $has_eventTime = EventService::hasEventDuplication($request['event_date'], $request['start_time'], $request['end_time']);
@@ -68,12 +52,6 @@ class EventController extends Controller
         return to_route('events.index');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Event  $event
-     * @return \Illuminate\Http\Response
-     */
     public function show(Event $event)
     {
         $event = Event::findOrFail($event->id);
@@ -85,16 +63,10 @@ class EventController extends Controller
         compact('event', 'eventDate', 'startTime', 'endTime'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Event  $event
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Event $event)
     {
         $event = Event::findOrFail($event->id);
-        $eventDate = $event->eventDate;
+        $eventDate = $event->editEventDate;
         $startTime = $event->startTime;
         $endTime = $event->endTime;
 
@@ -102,24 +74,39 @@ class EventController extends Controller
         compact('event', 'eventDate', 'startTime', 'endTime'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateEventRequest  $request
-     * @param  \App\Models\Event  $event
-     * @return \Illuminate\Http\Response
-     */
     public function update(UpdateEventRequest $request, Event $event)
     {
-        //
+        $countEventDuplication = EventService::countEventDuplication($request['event_date'], $request['start_time'], $request['end_time']);
+
+        if($countEventDuplication > 1){
+
+            $event = Event::findOrFail($event->id);
+            $eventDate = $event->editEventDate;
+            $startTime = $event->startTime;
+            $endTime = $event->endTime;
+
+            session()->flash('status', 'その時間帯は既に他のイベントに予約されています');
+            return view('manager.events.edit',
+            compact('event', 'eventDate', 'startTime', 'endTime'));
+        }
+
+        $start_date = EventService::joinDateAndTime($request['event_date'], $request['start_time']);
+        $end_date = EventService::joinDateAndTime($request['event_date'], $request['end_time']);
+
+        $event = Event::findOrFail($event->id);
+        $event->name = $request['event_name'];
+        $event->information = $request['information'];
+        $event->max_people = $request['max_people'];
+        $event->start_date = $start_date;
+        $event->end_date = $end_date;
+        $event->is_visible = $request['is_visible'];
+        $event->save();
+
+        session()->flash('status', 'イベント情報の更新が完了しました');
+
+        return to_route('events.index');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Event  $event
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Event $event)
     {
         //
